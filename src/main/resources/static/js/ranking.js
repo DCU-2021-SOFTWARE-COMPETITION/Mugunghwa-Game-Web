7c43b95243b2ddd9824d7fdc0550291b9015a41f
seunggyun-jeong <jskyun98@gmail.com>
2021-11-17 20:48:52 +0900

    ranking test

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -3,7 +3,8 @@ let rankData;
 axios
     .get("/home/showRankUser")
     .then(function (response) {
-        rankData = console.log(response.data);
+        rankData = response.data;
+        console.log(response.data);
     }).catch(function (error) {
         console.log(error);
     });
