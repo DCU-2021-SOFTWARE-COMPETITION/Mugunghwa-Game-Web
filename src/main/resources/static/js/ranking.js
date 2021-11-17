bbecc6b4239f49adc3cc8eb0128a5e11b0a5c3d8
seunggyun-jeong <jskyun98@gmail.com>
2021-11-17 21:43:15 +0900

    ranking test

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -1,21 +1,25 @@
 let rankData;
-const parentList = document.querySelectork("#rankList");
+const parentList = document.querySelector("#rankList");
 
 axios
     .get("/home/showRankUser")
     .then(function (response) {
         rankData = response.data;
         console.log(response.data);
+        for (let i = 0; i < rankData.length; i++) {
+            let listName = document.createElement('ul');
+
+            let spanName = document.createElement('span');
+            spanName.innerText = rankData[i].userName;
+
+            let spanScore = document.createElement('span');
+            spanScore.innerText = rankData[i].recordScore;
+
+            listName.appendChild(spanName);
+            listName.appendChild(spanScore);
+
+            parentList.appendChild(listName);
+        }
     }).catch(function (error) {
         console.log(error);
     });
-
-for (let i = 0; i < rankData.length; i++) {
-    let listName = document.createElement('ul');
-    let listScore = document.createElement('ul');
-    listName.innerHTML(rankData[1].userName)
-    listScore.innerHTML(rankData[1].recordScore);
-
-    parentList.appendChild(list);
-}
-
