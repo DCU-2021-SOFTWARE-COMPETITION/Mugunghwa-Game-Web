3102827e5a438d5c428a1dcf9e667a31f1b46810
seunggyun-jeong <jskyun98@gmail.com>
2021-11-17 21:53:31 +0900

    ranking v0.1

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -6,20 +6,24 @@ axios
     .then(function (response) {
         rankData = response.data;
         console.log(response.data);
-        for (let i = 0; i < rankData.length; i++) {
-            let listName = document.createElement('ul');
-
-            let spanName = document.createElement('span');
-            spanName.innerText = rankData[i].userName;
-
-            let spanScore = document.createElement('span');
-            spanScore.innerText = rankData[i].recordScore;
-
-            listName.appendChild(spanName);
-            listName.appendChild(spanScore);
-
-            parentList.appendChild(listName);
-        }
+        loadList();
     }).catch(function (error) {
         console.log(error);
     });
+
+function loadList() {
+    for (let i = 0; i < rankData.length; i++) {
+        let listName = document.createElement('ul');
+
+        let spanName = document.createElement('span');
+        spanName.innerText = rankData[i].userName;
+
+        let spanScore = document.createElement('span');
+        spanScore.innerText = rankData[i].recordScore;
+
+        listName.appendChild(spanName);
+        listName.appendChild(spanScore);
+
+        parentList.appendChild(listName);
+    }
+}
